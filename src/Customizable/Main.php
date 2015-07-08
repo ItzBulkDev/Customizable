afd4acdc0b52265cbea5991cb961c86bdc334962
Bulk Developer <jaydenpatrick1110@gmail.com>
2015-07-08 19:19:38 -0400

    Update Main.php

--- src/Customizable/Main.php
+++ src/Customizable/Main.php
@@ -18,9 +18,9 @@ class Main extends PluginBase implements Listener{
 public function onEnable(){
 $this->saveDefaultConfig();
 $config = $this->getConfig();
-$this->getServer->getPluginManager->registerEvents($this, $this);
-$this->getServer->info(TextFormat::BLUE."Customizable" . TextFormat::YELLOW . ": " . TextFormat::GREEN . "Enabled!");
-$this->getServer->info(TextFormat::BLUE."Customizable" . TextFormat::YELLOW . ": " . TextFormat::GREEN . "Created by ItzBulkDev");
+$this->getServer()->getPluginManager->registerEvents($this, $this);
+$this->getLogger()->info(TextFormat::BLUE."Customizable" . TextFormat::YELLOW . ": " . TextFormat::GREEN . "Enabled!");
+$this->getLogger()->info(TextFormat::BLUE."Customizable" . TextFormat::YELLOW . ": " . TextFormat::GREEN . "Created by ItzBulkDev");
 $whitelistMsg = $config->get("Whitelist-Message");
 $bannedMsg = $config->get("Banned-Message");
 $fullMsg = $config->get("Server-Full-Message");
@@ -28,17 +28,17 @@ $fullMsg = $config->get("Server-Full-Message");
 
 public function onDisable(){
 $this->saveDefaultConfig();
-$this->getServer->info(TextFormat::BLUE."Customizable" . TextFormat::YELLOW . ": " . TextFormat::RED . "Disabled!");
-$this->getServer->info(TextFormat::BLUE."Customizable" . TextFormat::YELLOW . ": " . TextFormat::RED . "Created by ItzBulkDev");
+$this->getLogger()->info(TextFormat::BLUE."Customizable" . TextFormat::YELLOW . ": " . TextFormat::RED . "Disabled!");
+$this->getLogger()->info(TextFormat::BLUE."Customizable" . TextFormat::YELLOW . ": " . TextFormat::RED . "Created by ItzBulkDev");
 }
 
 public function onPlayerJoin(PlayerJoinEvent $event){
   $p = $event->getPlayer();
   $pn = $p->getName();
-    if($this->server->isWhitelisted(strtolower($p)) == false){
+    if($this->getServer()->isWhitelisted(strtolower($p)) == false){
     $p->kick($WhitelistMsg);
     }else{
-      if(count($this->server->getOnlinePlayers()) > $this->server->getMaxPlayers()){ 
+      if(count($this->getServer()->getOnlinePlayers()) > $this->getServer()->getMaxPlayers()){ 
         $p->kick($fullMsg);
       }
       }
