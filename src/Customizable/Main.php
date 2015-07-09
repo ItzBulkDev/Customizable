603ee104e7ae1438066e124d9391705a6c9e80ce
Bulk Developer <jaydenpatrick1110@gmail.com>
2015-07-08 21:17:34 -0400

    Update Main.php

--- src/Customizable/Main.php
+++ src/Customizable/Main.php
@@ -16,33 +16,3 @@ use pocketmine\event\player\PlayerKickEvent;
 
 class Main extends PluginBase implements Listener{
 
-public function onEnable(){
-$this->saveDefaultConfig();
-$config = $this->getConfig();
-$this->getServer()->getPluginManager()->registerEvents($this, $this);
-$this->getLogger()->info(TextFormat::BLUE."Customizable" . TextFormat::YELLOW . ": " . TextFormat::GREEN . "Enabled!");
-$this->getLogger()->info(TextFormat::BLUE."Customizable" . TextFormat::YELLOW . ": " . TextFormat::GREEN . "Created by ItzBulkDev");
-$whitelistMsg = $config->get("Whitelist-Message");
-$bannedMsg = $config->get("Banned-Message");
-$fullMsg = $config->get("Server-Full-Message");
-}
-
-public function onDisable(){
-$this->saveDefaultConfig();
-$this->getLogger()->info(TextFormat::BLUE."Customizable" . TextFormat::YELLOW . ": " . TextFormat::RED . "Disabled!");
-$this->getLogger()->info(TextFormat::BLUE."Customizable" . TextFormat::YELLOW . ": " . TextFormat::RED . "Created by ItzBulkDev");
-}
-
-public function onPlayerJoin(PlayerJoinEvent $event){
-  $p = $event->getPlayer();
-  $pn = $p->getName();
-    if($this->getServer()->isWhitelisted(strtolower($p)) == false){
-    $p->kick($WhitelistMsg);
-    }else{
-      if(count($this->getServer()->getOnlinePlayers()) > $this->getServer()->getMaxPlayers()){ 
-        $p->kick($fullMsg);
-      }
-      }
-}
-}
-
