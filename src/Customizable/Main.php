7ef0dd68258f2684c6f5a48264c18a19ad326b31
Bulk Developer <jaydenpatrick1110@gmail.com>
2015-07-08 21:17:47 -0400

    Update Main.php

--- src/Customizable/Main.php
+++ src/Customizable/Main.php
@@ -1,18 +1,3 @@
 <?php
 
-//Created By ItzBulkDev
-
-namespace Customizable;
-
-use pocketmine\Player;
-use pocketmine\Server;
-use pocketmine\utils\TextFormat;
-use pocketmine\utils\Config;
-use pocketmine\plugin\PluginBase;
-use pocketmine\event\Listener;
-use pocketmine\event\player\PlayerJoinEvent;
-use pocketmine\event\player\PlayerQuitEvent;
-use pocketmine\event\player\PlayerKickEvent;
-
-class Main extends PluginBase implements Listener{
 
